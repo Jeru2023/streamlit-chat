65daf15ae172a82139512a142327d36f9729223c
jeruliu <jeru.liu@yahoo.com>
2023-04-21 19:47:21 +0800

    Successfully chat to openai

--- app.py
+++ app.py
@@ -19,4 +19,25 @@ BASE_PROMPT = [{"role": "system", "content": "You are a helpful assistant."}]
 if "messages" not in st.session_state:
     st.session_state["messages"] = BASE_PROMPT
 
-st.header("STREAMLIT GPT-3 CHATBOT")
+st.header("Welcome to Jeru's CHATBOT")
+
+text = st.empty()
+show_messages(text)
+
+prompt = st.text_input("Prompt", value="Enter your message here...")
+
+if st.button("Send"):
+    with st.spinner("Generating response..."):
+        st.session_state["messages"] += [{"role": "user", "content": prompt}]
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=st.session_state["messages"]
+        )
+        message_response = response["choices"][0]["message"]["content"]
+        st.session_state["messages"] += [
+            {"role": "system", "content": message_response}
+        ]
+        show_messages(text)
+
+if st.button("Clear"):
+    st.session_state["messages"] = BASE_PROMPT
+    show_messages(text)
