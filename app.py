7177b7ddef8ee0e629fdda4343e4e5d912612c7c
jeruliu <jeru.liu@yahoo.com>
2023-04-21 21:06:55 +0800

    Added sidebar and proxy

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
 import openai
 import toml
 import streamlit as st
+import os
 
+st.set_page_config(page_title='ChatGPT Assistant', layout='wide', page_icon='🎭')
+
+with open("secrets.toml", "r") as f:
+    config = toml.load(f)
+
+openai.api_key = config["OPENAI_KEY"]
+os.environ["http_proxy"]="http://127.0.0.1:7890"
+os.environ["https_proxy"]="http://127.0.0.1:7890"
+
+BASE_PROMPT = [{"role": "system", "content": "You are a helpful assistant."}]
+
+with st.sidebar:
+	# st.header("Control Panel")
+	st.markdown("# 🎭 Control Panel")
+	temperature = st.radio(
+    "Select temperature 👇",
+    (0, 1, 2))
+	
+	uploaded_files = st.file_uploader("Choose a PDF file", accept_multiple_files=True)
+	for uploaded_file in uploaded_files:
+		bytes_data = uploaded_file.read()
+		st.write("filename:", uploaded_file.name)
+		st.write(bytes_data)
 
 def show_messages(text):
     messages_str = [
         f"{_['role']}: {_['content']}" for _ in st.session_state["messages"][1:]
     ]
     text.text_area("Messages", value=str("\n".join(messages_str)), height=400)
-
-
-with open("secrets.toml", "r") as f:
-    config = toml.load(f)
-
-openai.api_key = config["OPENAI_KEY"]
-BASE_PROMPT = [{"role": "system", "content": "You are a helpful assistant."}]
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = BASE_PROMPT
@@ -24,7 +41,7 @@ st.header("Welcome to Jeru's CHATBOT")
 text = st.empty()
 show_messages(text)
 
-prompt = st.text_input("Prompt", value="Enter your message here...")
+prompt = st.text_input("Prompt", placeholder="Enter your message here...")
 
 if st.button("Send"):
     with st.spinner("Generating response..."):
